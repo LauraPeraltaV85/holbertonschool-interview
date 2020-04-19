ca6e6e5066d00c4d7a8e5f59fdd1d2c748921b52
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-04-19 01:51:57 +0000

    [fix] condition

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,6 +3,8 @@
 def canUnlockAll(boxes):
     key = 0
     keychain = [0]
+    if len(boxes) is 0:
+        return True
     for key in keychain:
         for key in boxes[key]:
             if key in keychain:

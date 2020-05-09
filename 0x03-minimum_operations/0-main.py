95ca3fb2c65825f61326eeeb2e03025b7baa362e
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-05-09 23:27:35 +0000

    [feature] method to calculate fewest number of operations

---
+++ 0x03-minimum_operations/0-main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+
+n = 3
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 7
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 21
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 5
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 27
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 9
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 15
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+n = 8
+print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Method that calculates the fewest number of operations needed to result in exactly n H characters in the file
+"""
+
+
+def minOperations(n):
+    """This method returns the number of operations to copy paste"""
+    if n < 1:
+        return 0
+    char = 1
+    copy = 0
+    #copied = 0
+    pasted = 1
+    paste = 0
+    pre = 1
+    notcopy1 = 0
+    notcopy3 = 0
+    while char < n:
+        if n % (char * 2) == 0:
+            if notcopy1 == 1:
+                notcopy1 = 0
+            notcopy3 = 0
+            copy += 1
+            pre = char
+            paste += 1
+            char = 2 * char
+            #copied += ant
+            pasted += (2 * pre) - pasted
+        elif n % char == 0 and char != 1:
+            if n % pasted == 0:
+                pre = pasted
+                notcopy1 = 0
+                char = pasted
+            else:
+                pre = char
+            notcopy3 = 0
+            if notcopy1 == 0:
+                notcopy1 = 1
+                copy += 1
+            paste += 1
+            pasted += pre
+            # print("entra en por 1", co, pegados)
+        else:
+            notcopy1 = 0
+            if notcopy3 == 0:
+                notcopy3 = 1
+                copy += 1
+                pre = char
+            char = char + pre
+            paste += 1
+            pasted += pre
+            # print("entra en sumar 1", co, pegados)
+        if pasted >= n:
+            break
+    return copy + paste

e995b5d99b09248ea71c2b3a3dc7ddc69d40cea5
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-05-18 20:23:18 +0000

    [fix] betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -15,8 +15,7 @@ for (j = 0; j < 3; j++)
 {
 if (j)
 printf(" ");
-printf("%d", grid[i][j]);
-}
+printf("%d", grid[i][j]); }
 printf("\n");
 } }
 /**
@@ -39,13 +38,11 @@ for (j = 0; j < 3; j++)
 {
 grid2[i][j] = grid1[i][j];
 if (grid1[i][j] <= 3)
-stable++;
-} }
+stable++; } }
 if (stable == 9)
 break;
 printf("=\n");
-print_grid(grid1);
-}
+print_grid(grid1); }
 stable = 0;
 for (i = 0; i < 3; i++)
 {
@@ -57,19 +54,15 @@ if (grid2[i][j] > 3)
 {
 grid1[i][j] = grid1[i][j] - 4;
 if (j > 0)
-grid1[i][j-1] = (grid1[i][j-1]) + 1;
+grid1[i][j - 1] = (grid1[i][j - 1]) + 1;
 if (i > 0)
 grid1[i - 1][j] = (grid1[i - 1][j]) + 1;
 if (i < 2)
 grid1[i + 1][j] = (grid1[i + 1][j]) + 1;
 if (j < 2)
-grid1[i][j + 1] = (grid1[i][j + 1]) + 1;
-}
+grid1[i][j + 1] = (grid1[i][j + 1]) + 1; }
 else
-stable++;
-}
+stable++; }
 else
-grid1[i][j] = grid1[i][j] + grid2[i][j];
-} }
-flag = 1;
-} }
+grid1[i][j] = grid1[i][j] + grid2[i][j]; } }
+flag = 1; } }

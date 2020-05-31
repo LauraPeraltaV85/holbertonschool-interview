c844edb55c305ddd78b7de6993562f35d4060351
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-05-31 20:11:35 +0000

    [fix] except

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,27 +12,25 @@ statusn = [0, 0, 0, 0, 0, 0, 0, 0]
 size = 0
 statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-def signal_handler(signal, frame):
-    """ Method to handle ctrl-c"""
+try:
+    for line in fileinput.input():
+        each_argvs = line.split(" ")
+        if len(each_argvs) == 9:
+            status = each_argvs[-2]
+            if status in statuses:
+                i = statuses.index(status)
+                statusn[i] = statusn[i] + 1
+                size = size + int(each_argvs[-1][:-1])
+                if a % 10 == 0:
+                    print("File size: {}".format(size))
+                    for i in range(len(statusn)):
+                        if statusn[i] > 0:
+                            print("{}: {}".format(statuses[i], statusn[i]))
+        a = a + 1
+
+except KeyboardInterrupt:
     print("File size: {}".format(size))
     for i in range(len(statusn)):
         if statusn[i] > 0:
             print("{}: {}".format(statuses[i], statusn[i]))
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in fileinput.input():
-    each_argvs = line.split(" ")
-    if len(each_argvs) == 9:
-        status = each_argvs[-2]
-        if status in statuses:
-            i = statuses.index(status)
-            statusn[i] = statusn[i] + 1
-            size = size + int(each_argvs[-1][:-1])
-            if a % 10 == 0:
-                print("File size: {}".format(size))
-                for i in range(len(statusn)):
-                    if statusn[i] > 0:
-                        print("{}: {}".format(statuses[i], statusn[i]))
-    a = a + 1
+    raise

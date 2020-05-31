fb7c661559531082b38746e30dbe1717f1cdc513
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-05-31 23:41:14 +0000

    [fix] fileinput

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,30 +5,31 @@ number of status codes in a log list
 
 import sys
 
-a = 1
-statusn = [0, 0, 0, 0, 0, 0, 0, 0]
-size = 0
-statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
+if __name__ == "__main__":
+    a = 1
+    statusn = [0, 0, 0, 0, 0, 0, 0, 0]
+    size = 0
+    statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-def print_log(statusn, statuses):
-    """Method to print file size and status"""
-    print("File size: {}".format(size))
-    for i in range(len(statusn)):
-        if statusn[i] > 0:
-            print("{}: {}".format(statuses[i], statusn[i]))
+    def print_log(statusn, statuses):
+        """Method to print file size and status"""
+        print("File size: {}".format(size))
+        for i in range(len(statusn)):
+            if statusn[i] > 0:
+                print("{}: {}".format(statuses[i], statusn[i]))
 
-try: 
-    for line in sys.stdin:
-        each_argvs = line.split(" ")
-        if len(each_argvs) == 9:
-            status = each_argvs[-2]
-            if status in statuses:
-                i = statuses.index(status)
-                statusn[i] = statusn[i] + 1
-                size = size + int(each_argvs[-1][:-1])
-                if a % 10 == 0:
-                    print_log(statusn, statuses)
-        a = a + 1
-except KeyboardInterrupt:
-    print_log(statusn, statuses)
-    raise
+    try: 
+        for line in sys.stdin:
+            each_argvs = line.split(" ")
+            if len(each_argvs) == 9:
+                status = each_argvs[-2]
+                if status in statuses:
+                    i = statuses.index(status)
+                    statusn[i] = statusn[i] + 1
+                    size = size + int(each_argvs[-1][:-1])
+                    if a % 10 == 0:
+                        print_log(statusn, statuses)
+            a = a + 1
+    except KeyboardInterrupt:
+        print_log(statusn, statuses)
+        raise

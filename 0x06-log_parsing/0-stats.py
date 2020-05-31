28cc2d920fbc2c5955072aff340bb0b76ab5c7a3
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-05-31 21:09:20 +0000

    [fix] except

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,7 +12,14 @@ statusn = [0, 0, 0, 0, 0, 0, 0, 0]
 size = 0
 statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-try:
+def print_log(statusn, statuses):
+    print("File size: {}".format(size))
+    for i in range(len(statusn)):
+        if statusn[i] > 0:
+            print("{}: {}".format(statuses[i], statusn[i]))
+
+
+try: 
     for line in fileinput.input():
         each_argvs = line.split(" ")
         if len(each_argvs) == 9:
@@ -21,16 +28,9 @@ try:
                 i = statuses.index(status)
                 statusn[i] = statusn[i] + 1
                 size = size + int(each_argvs[-1][:-1])
-                if a % 10 == 0:
-                    print("File size: {}".format(size))
-                    for i in range(len(statusn)):
-                        if statusn[i] > 0:
-                            print("{}: {}".format(statuses[i], statusn[i]))
+            if a % 10 == 0:
+                print_log(statusn, statuses)
         a = a + 1
-
 except KeyboardInterrupt:
-    print("File size: {}".format(size))
-    for i in range(len(statusn)):
-        if statusn[i] > 0:
-            print("{}: {}".format(statuses[i], statusn[i]))
+    print_log(statusn, statuses)
     raise

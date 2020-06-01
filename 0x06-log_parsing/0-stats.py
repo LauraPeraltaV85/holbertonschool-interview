7db17f84fa2774e7dea941bf3f7ba34e3a0e680b
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-06-01 00:09:30 +0000

    [fix] try except

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,6 +5,7 @@ number of status codes in a log list
 
 import sys
 
+
 if __name__ == "__main__":
     a = 1
     statusn = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -20,20 +21,17 @@ if __name__ == "__main__":
 
     try: 
         for line in sys.stdin:
-            try:
-                each_argvs = line.split(" ")
-                if len(each_argvs) == 9:
-                    status = each_argvs[-2]
-                    if status in statuses:
-                        i = statuses.index(status)
-                        statusn[i] = statusn[i] + 1
-                        size = size + int(each_argvs[-1][:-1])
-            except:
-                pass
-            if a % 10 == 0:
-                print_log(statusn, statuses)
+            each_argvs = line.split(" ")
+            if len(each_argvs) == 9:
+                status = each_argvs[-2]
+                if status in statuses:
+                    i = statuses.index(status)
+                    statusn[i] = statusn[i] + 1
+                    size = size + int(each_argvs[-1][:-1])
+                    if a % 10 == 0:
+                        print_log(statusn, statuses)
             a = a + 1
     except KeyboardInterrupt:
         print_log(statusn, statuses)
         raise
-    #print_log(statusn, statuses)
+

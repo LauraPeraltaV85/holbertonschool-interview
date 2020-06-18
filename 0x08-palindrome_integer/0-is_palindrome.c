926b5c34c74651f223e7ad77f62a202d61e47b4b
LauraPeraltaV85 <lalaperalta85@gmail.com>
2020-06-18 18:44:45 +0000

    [fix}

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -9,26 +9,14 @@
 int is_palindrome(unsigned long n)
 {
 int unit = 0;
-unsigned long int dec = 1;
-unsigned long int dec_up = 1;
 unsigned long int revn = 0;
 unsigned long int original = n;
 
-while (n > dec)
-{
-dec = dec * 10;
-}
-if (dec < 1)
-return (1);
-dec = dec / 10;
-
 while (dec >= 1)
 {
-unit = n / dec;
-n = n - (unit *dec);
-revn = revn + (unit *dec_up);
-dec_up = dec_up * 10;
-dec = dec / 10;
+unit = n % 10;
+revn = (revn *(10)) + unit
+n = n / 10;
 }
 return (original == revn);
 }
